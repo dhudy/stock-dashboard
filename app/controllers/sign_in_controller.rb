dac60bcaaa04fb2f9e9180d94f117bf168cff28f
Daniel Hudy <djh3315@rit.edu>
2015-04-11 14:48:08 -0400

    GOD DAMN IM DUMB

--- app/controllers/sign_in_controller.rb
+++ app/controllers/sign_in_controller.rb
@@ -1,6 +1,9 @@
 class SignInController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+
+  end
   protected
 
   def auth_hash
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'sign_in#index'
+  root 'welcome#index'
   get 'auth/facebook/callback', to: 'sign_in#create'
   get 'auth/failure', to: 'sign_in#failure'
   get 'welcome', to: 'welcome#index'
