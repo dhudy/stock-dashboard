5757b85c5c6bc3f01fea305843f8caa105b8e779
Daniel Hudy <djh3315@rit.edu>
2015-04-11 13:56:36 -0400

    Facebook plx

---
+++ app/controllers/sign_in_controller.rb
@@ -0,0 +1,13 @@
+class SignInController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+
+  end
+
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+end
---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,16 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def facebook
+    # puts "--#{request.env['omniauth.auth'].inspect}"
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+    @user.auth_token = request.env['omniauth.auth']['credentials']['token']
+    @user.save
+    if @user.persisted?
+      sign_in @user
+      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+      redirect_to root_path
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+end
