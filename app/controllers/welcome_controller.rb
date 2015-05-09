2f8160e24a06e59a2abbdba3bca263d69098383d
Daniel Hudy <djh3315@rit.edu>
2015-05-09 11:50:58 -0400

    Facebook Feed completed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,7 +6,14 @@ class WelcomeController < ApplicationController
     unless current_user.nil?
       @user = current_user
       @graph = Koala::Facebook::API.new(@user.auth_token)
+      @posts = @graph.get_connection('me', 'home',
+                    {limit: 25,
+                      fields: ['message', 'id', 'from', 'type',
+                                'picture', 'link', 'created_time', 'updated_time'
+                        ]})
+      # raise @posts.inspect
 	  myself = @graph.get_object('me')
+	  # raise myself.inspect
     else
       @user = nil
     end
